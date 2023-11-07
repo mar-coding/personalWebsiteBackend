e3242d2b83dd8a4053cfda826b56429c41468bb1
marcoding-78 <marcoding78@gmail.com>
2023-11-07 23:34:28 +0330

    add: create default http server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("this is just a beginning")
+	fmt.Println("Begin")
+
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hi", getHi)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got request")
+	_, err := io.WriteString(w, "This is root of my webserver\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func getHi(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hi request")
+	_, err := io.WriteString(w, "This is hi route of my webserver\n")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
